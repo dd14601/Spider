12fa2fb6e26193ce182264714ebe3c70f7ca10de
peter.dong <584100723@qq.com>
2018-11-04 16:03:46 +0800

    Add files via upload
    TopsecIDS日志txt转xlsx

---
+++ TopIDS_log.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-  
+# @time     :    14:33
+# @Author   : Peter.Dong
+# File      : TopIDS_log.py
+
+from openpyxl import Workbook
+wb = Workbook()  # 创建工作簿
+ws = wb.active  # 创建sheet
+tag = ['时间','级别','事件号','次数','协议','源地址','源端口','接口','目的地址','目的端口','目的接口','动作','事件描述']
+ws.append(tag)
+with open('topsec.txt','r',encoding='UTF-8') as f:
+
+    for i in f.readlines():
+        log_line = []
+        line_ = i.split(',')
+        for line in line_:
+            m = str(line.split('=')[1])
+            log_line.append(m)
+        ws.append(log_line)
+wb.save('topsec_log.xlsx')
