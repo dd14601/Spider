0610c6afadb0a769cd2569882926e6ae9a0868e5
peter.dong <584100723@qq.com>
2019-01-21 15:10:34 +0800

    Update TopIDS_log.py

--- TopIDS_log.py
+++ TopIDS_log.py
@@ -9,7 +9,7 @@ wb = Workbook()  # 创建工作簿
 ws = wb.active  # 创建sheet
 tag = ['时间','级别','事件号','次数','协议','源地址','源端口','接口','目的地址','目的端口','目的接口','动作','事件描述']
 ws.append(tag)
-with open('topsec.txt','r',encoding='UTF-8') as f:
+with open('log.txt','r',encoding='UTF-8') as f:
 
     for i in f.readlines():
         log_line = []
